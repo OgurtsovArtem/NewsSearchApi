1690e470583e2f53dd83ee6e1d7ab19bfb60fc7a
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2020-11-13 01:32:28 +0700

    Reworked error messages

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 const isEmail = require('validator/lib/isEmail');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const { messages } = require('../config/massage');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
-    validate: [isEmail, 'invalid email'],
+    validate: [isEmail, messages.schemas.invalidEmail],
   },
   password: {
     type: String,
@@ -27,13 +28,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new Error(messages.schemas.incorrectData));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new Error(messages.schemas.incorrectData));
           }
 
           return user;
