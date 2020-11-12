7c600d996f458061eb530565c147944c4d7972f0
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2020-11-13 01:17:58 +0700

    The elements are moved to separate modules

--- app.js
+++ app.js
@@ -1,58 +1,25 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
-const helmet = require('helmet');
-const { auth } = require('./middlewares/auth');
+const middlewares = require('./middlewares');
+const routers = require('./routes');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const routerUsers = require('./routes/users');
-const routerArticles = require('./routes/articles');
-const routerSingInUp = require('./routes/sing-in-up');
-const routerTest = require('./routes/server-test');
-const NotFoundError = require('./erros/404-not-found-err');
 
-const { PORT = 3000, MONGO_SERVER } = process.env;
+const { dataBaseLink, dataBaseConfig } = require('./config/database');
+const { errHandler } = require('./middlewares/centralized-errors-handler');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(middlewares);
 
-mongoose.connect(MONGO_SERVER, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
+mongoose.connect(dataBaseLink, dataBaseConfig);
 
-app.use('/', routerTest);
-app.use(helmet());
 app.use(requestLogger);
-app.use(limiter);
-app.use('/', routerSingInUp);
-app.use(auth);
-app.use('/', routerArticles);
-app.use('/', routerUsers);
-app.use('*', () => {
-  throw new NotFoundError('Запрашиваемый ресурс не найден');
-});
+app.use(routers);
 app.use(errorLogger);
 app.use(errors());
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Что-то пошло не так' } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-});
+app.use(errHandler);
 app.listen(PORT);
