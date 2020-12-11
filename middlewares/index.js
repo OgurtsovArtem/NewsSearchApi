3a9a08e15789d2a0aa43da279fd75fd88ef4fff8
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2020-12-11 22:12:50 +0700

    change cors place

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,8 +5,9 @@ const helmet = require('helmet');
 const limiter = require('./rate-limit');
 const bodyParser = require('./body-parser');
 
-router.use(cors);
+
 router.use(helmet());
+router.use(cors);
 router.use(bodyParser);
 router.use(limiter);
 
