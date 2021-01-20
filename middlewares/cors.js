8e907c03c0890d69ad27d49befc354b345e58ac4
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-01-20 19:13:31 +0700

    Rollback

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,26 +1,16 @@
 const router = require('express').Router();
 const cors = require('cors');
 
-const urlList = [
-  'http://localhost:8080',
-  'https://ogurtsovartem.github.io',
-  'https://newsartemogurtsov.students.nomoreparties.xyz',
-  'https://www.newsartemogurtsov.students.nomoreparties.xyz',
-  'http://newsartemogurtsov.students.nomoreparties.xyz',
-  'http://www.newsartemogurtsov.students.nomoreparties.xyz',
-]
 const corsOptions = {
-  origin: function (origin, callback) {
-    if(urlList.indexOf(origin) !== -1 || !origin){
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-    
-  },
-  credentials: true,
-  allowedHeaders: 'Content-Type',
+  origin: [
+  'https://ogurtsovartem.github.io',
+  'http://localhost:8080',
+  'http://newsproject.students.nomoreparties.xyz',
+  'http://localhost:3000',
+  'http://localhost:80',
+],
   optionsSuccessStatus: 200,
+  credentials: true,
 };
 
 router.use(cors(corsOptions));
