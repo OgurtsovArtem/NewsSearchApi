4a4bdd0ffed253bef8cc0eeeafbe2e4214e26a86
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-01-20 20:38:06 +0700

    fetch settings

--- routes/sing-in-up.js
+++ routes/sing-in-up.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 Joi.objectId = require('joi-objectid')(Joi);
 const { login, createUser } = require('../controllers/users');
 
-router.post('/signup', celebrate({
+router.post('/api/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     password: Joi.string().required().regex(/^[^ ]*$/).min(8),
@@ -11,7 +11,7 @@ router.post('/signup', celebrate({
   }),
 }), createUser);
 
-router.post('/signin', celebrate({
+router.post('/api/signin', celebrate({
   body: Joi.object().keys({
     password: Joi.string().required().min(8),
     email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: true } }),
