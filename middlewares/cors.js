5f8a312da0687333271b0d01e6fb6a91a80e64b1
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-01-20 19:17:49 +0700

    link correction

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,10 @@ const corsOptions = {
   origin: [
   'https://ogurtsovartem.github.io',
   'http://localhost:8080',
-  'http://newsproject.students.nomoreparties.xyz',
+  'https://www.newsartemogurtsov.students.nomoreparties.xyz/',
+  'https://newsartemogurtsov.students.nomoreparties.xyz/',
+  'http://www.newsartemogurtsov.students.nomoreparties.xyz/',
+  'http://newsartemogurtsov.students.nomoreparties.xyz/',
   'http://localhost:3000',
   'http://localhost:80',
 ],
