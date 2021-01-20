dd43740a3645bdf999282d255b4d91a8a1eb05ac
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-01-20 22:04:02 +0700

    link correction

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 
 const { getUser } = require('../controllers/users');
 
-router.get('api/users/me', celebrate({
+router.get('/api/users/me', celebrate({
   headers: Joi.object().keys({
     authorization: Joi.string().required().regex(/([a-zA-z]+)?\d+([a-zA-z]+)?/),
   }).unknown(true),
